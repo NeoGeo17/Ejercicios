39aee555fd6b6ed589323feae2672d6413d939b8
neoneo95 <emperador1833@gmail.com>
2018-04-29 10:48:32 -0300

    Creación de la clase Computer

---
+++ Computer.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Computer {
+
+    private static Scanner sc =  new Scanner(System.in);
+
+    public static void me(){
+        
+	//Rango de numeros
+	int min = 1;
+	int max = 100;
+
+	String answer = "";
+
+	System.out.println("Ingresa (+) si es mayor, (-) si es menor, (=) si es el numero:");
+
+	while(true){
+            //Punto medio de partida
+	    int computer = (max + min) / 2;
+
+	    System.out.printl("Es tu numero el " + computer + "?");
+	    answer = sc.nextLine();
+
+	    if(answer.equals("=")){
+
+                System.out.println("*** I win!!!. ***");
+		break;
+	    }
+	}
+    }
+
+}
