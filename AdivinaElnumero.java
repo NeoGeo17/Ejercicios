85095e8d909977d73c98697e8a1c703157f1285b
neoneo95 <emperador1833@gmail.com>
2018-04-29 11:47:09 -0300

    Terminando la clase Principal

--- AdivinaElnumero.java
+++ AdivinaElnumero.java
@@ -1,10 +1,36 @@
-
+import java.util.Scanner;
 
 public class AdivinaElnumero {
 
     public static void main(String [] args){
     
 	System.out.println("Bienvenido al juego, de adivina  el numero!!!");
+
+
+	do {
+         
+	    init();
+	}while(true);
+
+
+    }
+
+    public static void init(){
+	String option = null;
+	Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe \"me\" si quieres que adivine tu numero, en caso contrario lo hare yo:");
+        option = sc.nextLine();
+
+	if(option.equals("me")){
+            Computer.me();
+	}else {
+	    Human.man();
+	}
+
+
+
+
     }
 
 }
