ee979ed1f29f2faaf63d5ef83f745324e90fd97b
neoneo95 <emperador1833@gmail.com>
2018-04-29 10:13:59 -0300

    Creacion de la clase Human

---
+++ Human.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Human {
+
+    private static Random rand =  new Random(System.nanoTime());
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void man(){
+
+	int secretNumber = 0;
+	int input = 0;
+
+	//Rango de numeros
+	int min = 1;
+	int max = 100;
+
+	//Inicialiacion del numero secreto
+	secretNumber = rand.nextInt(100) + 1;
+
+	while(true){
+	    System.out.println("Elige un numero del 1 al 100:");
+
+	    input = Integer.parseInt(sc.nextLine());
+
+	    if(input > max || input < min){
+		System.out.println("Estas fuera del rango indicado!!!.");
+	    }
+	    if(input == secretNumber){
+                System.out.println("*** You win!!!. ***");
+		break;
+	    }
+	    if(secretNumber > input){
+
+                System.out.println("El numero secreto es mas alto.");
+	    }
+
+	    if(secretNumber < input){
+
+                System.out.println("El numero secreto es mas bajo.");
+	    }
+	}
+
+    }
+
+}
